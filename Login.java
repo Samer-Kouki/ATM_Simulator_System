4602716904a24b2c2fc30eb903b3a5a227d32053
Samer-Kouki <m.sc.samer.kouki@gmail.com>
2025-06-12 18:33:39 +0200

    Signup + jar

--- Login.java
+++ Login.java
@@ -1,3 +1,4 @@
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -12,7 +13,7 @@ public class Login extends JFrame implements ActionListener{
     Login(){
         setTitle("AUTOMATED TELLER MACHINE");
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/logo.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l11 = new JLabel(i3);
